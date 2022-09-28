b29b60c877e99a26622350ec2d61fac6ce1d7c4d
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-09-28 21:51:16 +1000

    Simplify one line in js script

--- js/doc-version-switcher.js
+++ js/doc-version-switcher.js
@@ -55,8 +55,7 @@ function generateVersionSwitcher(selector, lang, doctype) {
             // API docs are now a subfolder of the main docs. However,
             // to help the user here we still link the API subpage if
             // the user comes from a different API version
-            let dest_is_pre_010 = version_is_pre_010(version);
-            if (dest_is_pre_010) {
+            if (version_is_pre_010(version)) {
                 href = '/' + lang + '/' + doctype + version + '/';
             } else {
                 if (doctype === "api/") {
