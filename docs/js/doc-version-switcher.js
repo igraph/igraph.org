5960def72fb8e1b1a46b360b5396981d717f7e88
Tamas Nepusz <ntamas@gmail.com>
2022-09-29 16:54:39 +0200

    feat: added R 1.3.5 docs, updated Python docs to fix broken links, thanks @iosonofabio

--- docs/js/doc-version-switcher.js
+++ docs/js/doc-version-switcher.js
@@ -1,8 +1,19 @@
 let igraph_doc_versions = {
     c: '0.9.0 0.9.4 0.9.5 0.9.6 0.9.7 0.9.8 0.9.9 0.9.10 0.10.0 0.10.1 master develop',
-    r: '1.2.3 1.2.4 1.2.5 1.2.6 1.2.7 1.3.0 1.3.1 1.3.2 1.3.3 1.3.4',
+    r: '1.2.3 1.2.4 1.2.5 1.2.6 1.2.7 1.3.0 1.3.1 1.3.2 1.3.3 1.3.4 1.3.5',
     python: '0.9.6 0.9.7 0.9.8 0.9.9 0.9.10 0.9.11 0.10.0 0.10.1 master develop',
 };
+
+function version_is_pre_010(version) {
+    if (!version.includes(".")) {
+        return false;
+    }
+    let version_parts = version.split(".");
+    if (version_parts[0] !== "0") {
+        return false;
+    }
+    return parseInt(version_parts[1]) < 10
+}
 
 function generateVersionSwitcher(selector, lang, doctype) {
     let i, n;
@@ -34,7 +45,21 @@ function generateVersionSwitcher(selector, lang, doctype) {
         let version = versions[i];
         if (version && typeof version == 'string' && version.length > 0) {
             let child = document.createElement("a");
-            let href = '/' + lang + '/' + doctype + version + '/';
+            let href;
+
+            // Post 0.10 python docs change the location of the docs:
+            // API docs are now a subfolder of the main docs. However,
+            // to help the user here we still link the API subpage if
+            // the user comes from a different API version
+            if (version_is_pre_010(version)) {
+                href = '/' + lang + '/' + doctype + version + '/';
+            } else {
+                if (doctype === "api/") {
+                    href = '/' + lang + '/versions/' + version + '/' + doctype;
+                } else {
+                    href = '/' + lang + '/versions/' + version + '/';
+                }
+            }
 
             child.className = 'dropdown-item';
             child.appendChild(document.createTextNode(version));
